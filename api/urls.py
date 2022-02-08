a77fa9497184c5c0047c384281b0a973440628b7
LDraco-Sharif <shariful.islam.msi47@gmail.com>
2022-02-08 14:43:44 +0600

    simple jwt inclusion

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 router = routers.DefaultRouter()
 router.register(r'surveyors', views.SurveyorViewSet)
@@ -15,5 +20,8 @@ router.register(r'survey_answer', views.Survey_AnswerViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
