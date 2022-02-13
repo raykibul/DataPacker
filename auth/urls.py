65b1864d9053f443772ff5d48215787c21ec6c72
Rakibul islam <rakibcuet16@gmail.com>
2022-02-13 13:17:23 +0600

    auth app deleted

--- auth/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path,include
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('login', views.login, name='login'),
-    path('signup', views.login, name='signup'),
-]
