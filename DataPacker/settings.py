3a2ae353369235338295dbfb6bd095b970c2ed1b
Rakibul islam <rakibcuet16@gmail.com>
2022-02-14 13:30:52 +0600

    updated with statics files

--- DataPacker/settings.py
+++ DataPacker/settings.py
@@ -162,6 +162,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[str(BASE_DIR) +"/assets",]
+STATIC_ROOT=str(BASE_DIR) +'/static'
+MEDIA_ROOT=str(BASE_DIR) +'/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
