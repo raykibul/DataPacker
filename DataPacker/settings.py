f62dc68d1a3d530ac296bb74a8bf945bb388b2a9
Rakibul islam <rakibcuet16@gmail.com>
2022-02-14 19:23:55 +0600

    setting jwt token time extended

--- DataPacker/settings.py
+++ DataPacker/settings.py
@@ -85,7 +85,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10080),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
